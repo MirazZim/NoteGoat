2d36ec947456c2b300d08f09b09f5f756b2b62cd
MirazZim <miraz.zim.38@gmail.com>
2025-05-16 10:44:41 +0600

    Added ShadCn,Dark Mode and Toast

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import "./styles/globals.css";
+import { ThemeProvider } from "@/providers/ThemeProvider";
+import { Toaster } from "@/components/ui/sonner";
 
 export const metadata: Metadata = {
   title: "Note GOAT"
@@ -11,9 +13,18 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
-        {children}
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          {children}
+
+          <Toaster/>
+        </ThemeProvider>
       </body>
     </html>
   );
