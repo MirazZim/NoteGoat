db11856046ed982937d068e32fc590cde72bb3a6
MirazZim <miraz.zim.38@gmail.com>
2025-05-21 18:47:12 +0600

    Deployment 6

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,44 +7,24 @@ import AskAIButton from '@/components/AskAIButton';
 import NewNoteButton from '@/components/NewNoteButton';
 import NoteTextInput from '@/components/NoteTextInput';
 
-export default async function HomePage({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined }
-}) {
-  const user = await getUser();
-  if (!user) {
-    redirect("/login");
-  }
 
-  const noteIdParam = searchParams.noteId;
+type Props = {
+  searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
+};
+export default async function HomePage({ searchParams }: Props) {
+  const noteIdParam = (await searchParams).noteId;
+  const user = await getUser();
+
   const noteId = Array.isArray(noteIdParam)
-    ? noteIdParam[0]
+    ? noteIdParam![0]
     : noteIdParam || "";
 
-  let note = null;
-
-  if (noteId) {
-    note = await prisma.note.findUnique({
-      where: { id: noteId, authorId: user.id },
-    });
-  }
+  const note = await prisma.note.findUnique({
+    where: { id: noteId, authorId: user?.id },
+  });
 
   if (!note) {
-    note = await prisma.note.findFirst({
-      where: { authorId: user.id },
-      orderBy: { createdAt: "desc" },
-    });
-
-    if (!note) {
-      note = await prisma.note.create({
-        data: {
-          authorId: user.id,
-          text: "",
-        },
-      });
-    }
-    redirect(`/?noteId=${note.id}`);
+    redirect("/?noteId=${note.id}");
   }
 
   return (
